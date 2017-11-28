8723ef4d2df1ae4cd55312a9623d73d530eaf0f7
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 05:59:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
-    return array << "and #{array[-1]}"
+    return array.join(" and ")
   elsif array.length > 2
     array[0...-1].join(", ") << ", and #{array[-1]}"
     #array.each_with_index {|word,index|
