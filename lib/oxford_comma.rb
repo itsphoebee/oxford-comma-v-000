9e220c88baaefbf8937f4653d8263c79fe285b75
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 05:23:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
-    return array.insert(2," and ").join
+    return array.insert(1," and ").join
   #elsif array.length > 2
     #array.insert(-2," and ").join(", ")
     #array.each_with_index {|word,index|
