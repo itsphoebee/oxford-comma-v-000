0b2621471463c71d66599ef6662f4cd123c86143
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 05:01:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
-    return array.insert(-2," and ")
+    return array.insert(-2," and "),join
   elsif array.length > 2
     return array.insert(-2," and ").join(", ")
   end
