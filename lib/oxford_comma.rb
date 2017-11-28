22e828c0b27a6677791b18cfabcf4cde00d804da
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 06:03:02 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length > 2
-    array[0...-1].join(", ") << ", and #{array[-1]}" 
-    # selects all elements from array but the last one to join with ,
-    # includes , and final element of array 
+    array[0...-1].join(", ") << ", and #{array[-1]}"
+    # selects all elements from array but the last one to become a string with , in between each previous element
+    # includes , and final element of array as a string into the newly created string
   end
 end
